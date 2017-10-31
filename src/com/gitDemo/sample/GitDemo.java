a535bc04c51515975076c028bed73f5036c20df4
kumarhari187 <kumarhari187@gmail.com>
2017-10-31 14:30:10 +0530

    #1 : Bugfix : This is first commit into issue

--- src/com/gitDemo/sample/GitDemo.java
+++ src/com/gitDemo/sample/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Git Demo");
+		System.out.println("Git Demo issue_1");
 		
 	}
 
